fcf088db6f0d6730ab2dfaf5c3997822f940634d
jleon253 <jleon253@gmail.com>
2020-10-02 12:30:33 -0500

    Se borra linea duplicada

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,7 +1,6 @@
-import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
 
 @Injectable({
