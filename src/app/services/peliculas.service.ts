119a4de47d64e119f8b2d1c2b5982d571e28228c
jleon253 <jleon253@gmail.com>
2020-10-02 12:28:41 -0500

    Obteniendo peliculas en cartelera
    Se crea el servicio que consumira el EndPoint del API de peliculas para traer las peliculas en cartelera
    
    Se crea el archivo .ts que contiene el tipado para la respuesta del EndPoint Peliculas en Cartelera
    
    Se ajusta app.module para implementar HttpClientModule permitiendo conectar con el EndPoint
    
    Se ajusta app.component.ts para inyectar el servicio, implementar el metodo, consumir el EndPoint y mostrar la informacion del API

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,17 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { CarteleraResponse } from '../interfaces/cartelera-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  constructor( private http: HttpClient) { }
+
+  getCartelera(): Observable<CarteleraResponse> {
+    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=35ce4b3c4f02f07fd912b162e8bd3bfb&language=es-ES&page=1');
+  }
+}
